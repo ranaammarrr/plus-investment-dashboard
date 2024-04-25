649ffcb6b8026dd85a2ebcf8e565120a02ee76f9
Rana Ammar Talat <77064590+ranaammarrr@users.noreply.github.com>
2024-04-25 20:48:46 +0500

    fixes 2.0

--- src/Theme/theme.ts
+++ src/Theme/theme.ts
@@ -5,6 +5,8 @@ export const theme = createTheme({
     palette: {
       primary: {
         main: '#001529' ,
+        // main: '#174138' ,
+
       },
       secondary: {
         main: '#d5d3d3',
